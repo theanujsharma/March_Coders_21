ebf05a4308edadc33462a1fe6eecc5bf50e777fa
theanujsharma <theanujsharma@outlook.com>
2021-05-20 21:25:08 +0530

    Error Message

--- 20 May/script.js
+++ 20 May/script.js
@@ -1,16 +1,31 @@
 const studentIN = document.getElementById("student_in")
 const studentOut = document.getElementById("student_out")
 const count = document.getElementById("count")
+const error = document.getElementById("error")
 let num = 0 // starting value 
 studentIN.addEventListener("click",
 function(){
     num = num + 1 //incrementing by 1 // adding things
-    count.innerHTML = num
+    if (num > 4)
+    {
+        count.innerHTML = num
+        error.innerHTML = "You have reached the limit"
+        num = 5
+    }
+    else
+    {
+        count.innerHTML = num 
+    }
 })
 studentOut.addEventListener("click",
 function(){
     num = num - 1 // decrement by 1 //subtract
-    count.innerHTML = num
+    if (num < 5)
+    {
+        count.innerHTML = num
+        error.innerHTML = ""
+    }
+    
 })
 
 
