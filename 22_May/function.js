2de39d2e2ca1a8031509a980cb7e615e0dfde146
theanujsharma <theanujsharma@outlook.com>
2021-05-21 18:15:10 +0530

    Onclick function added

---
+++ 22_May/function.js
@@ -0,0 +1,11 @@
+const body = document.getElementById("body")
+
+function changeColor()
+{
+    body.style.backgroundColor = "blue"
+    body.style.backgroundColor = "red"
+    body.style.backgroundColor = "pink"
+    body.style.backgroundColor = "green"
+    body.style.backgroundColor = "black"
+    body.style.backgroundColor = "grey"
+}
