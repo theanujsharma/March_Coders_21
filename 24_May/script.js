fb18fa340330ddadab078b3322c71ebdfe760433
theanujsharma <theanujsharma@outlook.com>
2021-05-25 17:08:40 +0530

    For Loop

---
+++ 24_May/script.js
@@ -0,0 +1,51 @@
+for (let count = 0; count < 21; count = count + 1)
+{
+    console.log(count)
+}
+
+// let student1 = "Bhavya"
+// console.log(student1)
+// let student2 = "Soumith"
+// console.log(student2)
+// let student3 = "Aswath"
+// console.log(student3)
+// let student4 = "Ijay"
+// console.log(student4)
+// let student5 = "Yasir"
+// console.log(student5)
+// let student6 = "Pawan"
+// console.log(student6)
+// let student7 = "Rehan"
+// console.log(student7)
+
+//12 Lines
+
+// Array //Collection of same things
+// let students = ["Bhavya","Soumith", "Ashwat", "Rehan", "Pawan", "Yazir"]
+
+// for (let index = 0; index < students.length; index++)
+// {
+//     console.log(students[index])
+// }
+
+// console.log(students.length)
+// console.log(students[0])
+// console.log(students[1])
+// console.log(students[2])
+
+// let friends = ["Bhavya", "Ashwat", "Soumith", "Pawan"]
+// for (let index = 0; index < friends.length; index++)
+// {
+//     document.write("Hello,"+friends[index]+". "+"How are you? Please come to my party"+"<br/>")
+// }
+
+// let fruits = ["Apple", "Banana", "Kiwi", "Mango","Pineapple"]
+// for (let index = 0; index < fruits.length; index++)
+// {
+//     document.write(fruits[index]+"<br/>")
+// }
+for (let count = 0; count <1000000; count++)
+{
+    document.write("Bye"+"<br/>")
+}
+
